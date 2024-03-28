d484a50a4230ce9086dd0a3aad1dc8dd6a9c9834
Andy Nguyen <dokkandb@colostate.edu>
2024-03-28 14:37:09 -0600

    Initial Commit

---
+++ Assets/Rhythm Game Tutorial/Scripts/FallingArrow.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class FallingArrow : MonoBehaviour{
+    // Start is called before the first frame update
+    public float beatTempo;
+    public bool hasStarted;
+    void Start(){
+        beatTempo = beatTempo / 60f;
+    }
+
+    // Update is called once per frame
+    void Update(){
+        if (!hasStarted){
+            
+            // if (Input.anyKeyDown){
+            //     hasStarted = true;
+            // }
+        }
+
+        else {
+            transform.position -= new Vector3(0f, beatTempo * Time.deltaTime, 0f);
+
+        }
+        
+    }
+}
